790eb89318c8e0024c44d00a78b527f9d57448b0
benitotagle <benitotagle200926@tamu.edu>
2025-04-19 23:55:41 -0500

    Add files via upload
    Matlab file, forgot to upload it properly

---
+++ Project 4/Week1/Project 4 - Week 3/project4_main.m
@@ -0,0 +1,206 @@
+% ---------------- Vehicle Parameters ----------------
+inertia = 2000;         % [kg·m²] - Vehicle yaw inertia
+mass = 1600;            % [kg] - Vehicle mass
+rw = 0.3;               % [m] - Wheel radius
+vm = 1600;              % [kg] - (Unused duplicate of mass)
+iw = 0.5 * 7 * rw^2;    % [kg·m²] - Wheel moment of inertia (7 kg wheel)
+ipt = 10;               % [kg·m²] - Driveline/powertrain inertia
+cd = 0.36;              % [-] - Aerodynamic drag coefficient
+ad = 2.6;               % [m²] - Frontal area
+tau_brakes = 50000;     % [Nm] - Max brake torque
+tau_motor = 800;        % [Nm] - Max drive torque
+gratio = 3;             % [-] - Gear ratio (motor to wheel)
+i_gear_i = 0.5;         % [kg·m²] - Gear input inertia
+o_gear_i = 0.7;         % [kg·m²] - Gear output inertia
+c0 = 0.0041;            % [N] - Rolling resistance constant
+c1 = 0.000066;          % [N/(m/s)] - Speed-dependent rolling resistance
+
+
+%% Battery Parameters %%
+v_batt = 350;           % [V] - Nominal battery voltage
+r_internal = 0.1695;    % [Ohms] - Internal resistance
+eta_motor = 0.9;        % [-] - Motor efficiency
+tau_motor_max = 800;    % [Nm] - Max motor torque
+soc_min = 0.1;          % [-] - Minimum State of Charge
+b_capacity = 150;       % [Ah] - Battery pack capacity
+tau_regen_max = 300;    % [Nm] - Max regenerative braking torque
+SOC=[0, 0.01,   .1,  .2,  .3,  .4,  .5,  .6, .7,  .8,  .9,   1];
+OCV=[0, 3.1,3.55,3.68,3.74,3.76,3.78,3.85,3.9,3.95,4.08,4.15]; % [V] - Open Circuit Voltage per cell
+numSeries = 96;         % [-] - Series cells
+numParallel = 74;       % [-] - Parallel cells
+
+%% Environment %%
+rho = 1.225;
+
+% Vehicle Tire Info
+calpha_front = 40000;                  % [N/rad] - Front cornering stiffness
+calpha_rear = 40000;                   % [N/rad] - Rear cornering stiffness
+fyfrontmax = calpha_front * deg2rad(1);% [N] - Max front lateral force
+fyrearmax = calpha_rear * deg2rad(1);  % [N] - Max rear lateral force
+lf = 1.0;                              % [m] - Distance from CG to front axle
+lr = 1.5;                              % [m] - Distance from CG to rear axle
+radius = 0.3;                          % [m] - (possibly duplicate of rw)
+wheelbase = lf + lr;                  % [m] - Total wheelbase
+
+
+% Initial Conditions
+x0 = 0;
+y0 = 0;
+vx0 = 0.1;
+vy0 = 0;
+omega0 = 0;
+psi0 = 0;
+
+% ---------------- Motor Torque Curve ----------------
+motor_rpm = [0, 1000, 2000, 3000, 4000, 5000, 6000, ...
+             7000, 8000, 9000, 10000, 11000, 12000];
+motor_torque = [280, 280, 275, 260, 250, 230, 200, 175, 140, 120,  100,   75,     0];
+motor_torque_curve = [motor_rpm', motor_torque'];
+tau_motor_max = max(motor_torque);
+
+track_radius = 200;
+understeerCoeff = mass / ((lr + lf) * track_radius) * (lr / calpha_front - lf / calpha_rear);
+maxAlpha = 4 / 180 * pi;
+max_motor_rpm = max(motor_rpm);                     % [RPM]
+max_motor_radps = max_motor_rpm * 2 * pi / 60;      % [rad/s]
+vxd = (max_motor_radps / gratio) * rw;              % [m/s] computed from max rpm and wheel radius
+vx_threshold1 = 0.1;
+
+
+
+
+%% Controller%%
+kp = 0.0075; %0.0075
+ki = 0.02; %0.02
+kd = 0.1; %0.005
+corner_factor=0.99;
+lookahead_dist = 50;
+lookahead_dist_long=100;
+T_lookahead=0.3;
+
+% ---------------- Track Generation Parameters ----------------
+radius = 200; 
+l_st = 900; 
+width = 15; 
+
+l_curve = pi * radius;
+total_length = 2 * l_st + 2 * l_curve;
+
+delta_s = 1;
+npts = round(total_length / delta_s);
+delta_s = total_length / (npts - 1);
+delta_theta = delta_s / radius;
+
+% Preallocate path arrays
+xpath = zeros(npts,1);
+ypath = zeros(npts,1);
+tpath = zeros(npts,1);
+xinpath = zeros(npts,1);
+yinpath = zeros(npts,1);
+xoutpath = zeros(npts,1);
+youtpath = zeros(npts,1);
+
+yinpath(1) = 7.5;
+youtpath(1) = -7.5;
+
+% Track generation loop
+i = 1;
+while i < npts
+    if xpath(i) < l_st
+        if xpath(i) >= 0
+            if ypath(i) < radius
+                xpath(i+1) = xpath(i) + delta_s;
+                ypath(i+1) = ypath(i);
+                xinpath(i+1) = xinpath(i) + delta_s;
+                yinpath(i+1) = yinpath(i);
+                xoutpath(i+1) = xoutpath(i) + delta_s;
+                youtpath(i+1) = youtpath(i);
+                tpath(i+1) = 0;
+            else
+                xpath(i+1) = xpath(i) - delta_s;
+                ypath(i+1) = ypath(i);
+                xinpath(i+1) = xinpath(i) - delta_s; 
+                yinpath(i+1) = yinpath(i);
+                xoutpath(i+1) = xoutpath(i) - delta_s; 
+                youtpath(i+1) = youtpath(i);
+                tpath(i+1) = pi;
+            end
+        else
+            cx = 0; cy = radius;
+            tt = rotate([xpath(i)-cx; ypath(i)-cy], delta_theta);
+            ttin = rotate([xinpath(i)-cx; yinpath(i)-cy], delta_theta);
+            ttout = rotate([xoutpath(i)-cx; youtpath(i)-cy], delta_theta);
+            xpath(i+1) = tt(1) + cx; 
+            ypath(i+1) = tt(2) + cy;
+            xinpath(i+1) =  ttin(1) + cx; 
+            yinpath(i+1) = ttin(2) + cy;
+            xoutpath(i+1) = ttout(1) + cx; 
+            youtpath(i+1) = ttout(2) + cy;
+            tpath(i+1) = tpath(i) + delta_theta;
+        end
+    else
+        cx = l_st; cy = radius;
+        tt = rotate([xpath(i)-cx; ypath(i)-cy], delta_theta);
+        ttin = rotate([xinpath(i)-cx; yinpath(i)-cy], delta_theta);
+        ttout = rotate([xoutpath(i)-cx; youtpath(i)-cy], delta_theta);
+        xpath(i+1) = tt(1) + cx; 
+        ypath(i+1) = tt(2) + cy;
+        xinpath(i+1) =  ttin(1) + cx; 
+        yinpath(i+1) = ttin(2) + cy;
+        xoutpath(i+1) = ttout(1) + cx;
+        youtpath(i+1) = ttout(2) + cy;
+        tpath(i+1) = tpath(i) + delta_theta;
+    end
+    i = i + 1;    
+end
+disp(psi0)
+
+% ---------------- Run Simulation ----------------
+simout = sim("Project4_Simulink.slx");
+car_X = simout.X.Data;
+car_Y = simout.Y.Data;
+car_psi = simout.psi.Data;
+car_time = simout.tout;
+car_velocity=simout.velocity.Data;
+
+figure;
+scatter_plot = scatter(car_X, car_Y, 15, car_velocity, 'filled'); % 15 is marker size
+axis equal;
+xlabel('X Position (m)');
+ylabel('Y Position (m)');
+title('Vehicle Path Colored by Velocity');
+cb = colorbar;
+cb.Label.String = 'Velocity (m/s)';
+grid on;
+% ---------------- Animate Vehicle ----------------
+fh = figure();
+fh.WindowState = 'maximized';
+hold on
+plot(xpath, ypath, '--r'); axis equal;
+plot(xinpath, yinpath, 'b');
+plot(xoutpath, youtpath,'b');
+axis([min(xoutpath), max(xoutpath), min(youtpath), max(youtpath)])
+xlabel('X Distance (m)')
+ylabel('Y Distance (m)')
+title('Project 2 Track')
+grid on
+h = animatedline;
+
+L = 15;
+width = 5;
+for i = 1:length(car_X)
+    x = car_X(i);
+    y = car_Y(i);
+    psi = car_psi(i);
+    addpoints(h,x,y)
+
+    car = [-L/2 -width/2; -L/2 width/2; L/2 width/2; L/2 -width/2];
+    rcar = rotate(car', psi)';
+    a = polyshape(rcar + [x,y]);
+    ap = plot(a);
+    drawnow limitrate
+    ap.FaceColor = 'k';
+    drawnow limitrate 
+    pause(0.01);
+    delete(ap)
+end
